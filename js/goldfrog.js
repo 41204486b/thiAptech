4ae19f7449d11ac3a4142b94fd53921daff19dee
Việt <vietquocbach@gmail.com>
2017-11-13 00:07:15 +0700

    a

--- js/goldfrog.js
+++ js/goldfrog.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	function openTab(event, tabName){
-		var i, tabContent, tablinks;
-		alert($('.tabcontent').length);
-		alert('a');
-		// for( i = 0; i < $('.tabcontent').length; i++){
-		// 	alert()
-		// }
-	}
+	$('ul#menu_products li a').click(function(){
+		var tab_id = $(this).parent().attr('data-tab');
+		$('ul#menu_products li').removeClass('active');
+		// $('ul#menu_products li').hide(2000);
+
+		$('.tabcontent').removeClass('active');
+		$(this).parent().addClass('active');
+		$("#" + tab_id).addClass('active');
+		$("#" + tab_id).show(2000);
+	})
 })
